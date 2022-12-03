275ba985174002475367a567c3521f6f80add557
MaxF1996 <maxeffect.apollo@gmail.com>
2022-12-03 23:06:05 +0200

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryItemsHtml = galleryItems
+  .map(imageItem => {
+    return `<div class="gallery__item"><a class="gallery__link" href="${imageItem.original}"><img class="gallery__image" src="${imageItem.preview}" data-source="${imageItem.original}" alt="${imageItem.description}"/></a></div>`;
+  })
+  .join('');
+
+gallery.insertAdjacentHTML('afterbegin', galleryItemsHtml);
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  gallery.addEventListener('keydown', event => {
+    event.code === 'Escape' ? instance.close() : {};
+  });
+});
